cd4fc2f2c0ac749db92365e45f4bf4a048ffcbcf
Spaceiii <timothee01meyer@gmail.com>
2025-09-11 13:47:08 +0200

    config rm

--- config/dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  googleClientID:
-    "1049352596783-00r7sdmrfq478u5lur6sk3mqbss9cv7r.apps.googleusercontent.com",
-  googleClientSecret: "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",
-  mongoURI: "mongodb://localhost:27017/newsqls5",
-  cookieKey: "BUTINFOS5"
-};
